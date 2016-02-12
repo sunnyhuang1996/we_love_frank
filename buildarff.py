2cfb6ebd517af27eb8bf3eef5aa2a84a2fe2fa8e
Fang <ailsafangtian@gmail.com>
2016-02-12 11:57:55 -0500

    update ba

--- buildarff.py
+++ buildarff.py
@@ -140,10 +140,11 @@ if __name__ == '__main__':
     char_token = 0 #token of only character
     num_sen = 0
     num_char = 0
-    tweet_count=0
+    tweet_count=-1
 
     for line in input_file.readlines()[1:]:   # iterates the rows of the file in orders
         if line.strip()=="<A=0>" or line.strip()=="<A=4>":
+            tweet_count+=1
             if tweet_count in classrange:
                 print str(tweet_count) + "------"
                 try:
@@ -160,7 +161,6 @@ if __name__ == '__main__':
                 char_token = 0 #token of only character
                 num_sen = 0
                 num_char = 0
-                tweet_count+=1
                 
             else:
                 continue
