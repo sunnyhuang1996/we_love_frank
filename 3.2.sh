af595b5496cb62952a1fd6def76c5c9e1e729c52
Fang <ailsafangtian@gmail.com>
2016-02-15 16:01:26 -0500

    add

--- 3.2.sh
+++ 3.2.sh
@@ -1,14 +1,24 @@
 #!/bin/sh
 
+python buildarff0.py train.twt train.arff 500
+sh /u/cs401/WEKA/infogain.sh > 3.2output.txt
+python buildarff0.py train.twt train.arff 1000
+sh /u/cs401/WEKA/infogain.sh >> 3.2output.txt
+python buildarff0.py train.twt train.arff 1500
+sh /u/cs401/WEKA/infogain.sh >> 3.2output.txt
+python buildarff0.py train.twt train.arff 2000
+sh /u/cs401/WEKA/infogain.sh >> 3.2output.txt
 python buildarff0.py train.twt train.arff 2500
-java -cp /u/cs401/WEKA/weka.jar weka.classifiers.bayes.NaiveBayes -t train.arff -T test.arff -o >> 3.2output.txt
+sh /u/cs401/WEKA/infogain.sh >> 3.2output.txt
 python buildarff0.py train.twt train.arff 3000
-java -cp /u/cs401/WEKA/weka.jar weka.classifiers.bayes.NaiveBayes -t train.arff -T test.arff -o >> 3.2output.txt
+sh /u/cs401/WEKA/infogain.sh >> 3.2output.txt
 python buildarff0.py train.twt train.arff 3500
-java -cp /u/cs401/WEKA/weka.jar weka.classifiers.bayes.NaiveBayes -t train.arff -T test.arff -o >> 3.2output.txt
+sh /u/cs401/WEKA/infogain.sh >> 3.2output.txt
 python buildarff0.py train.twt train.arff 4000
-java -cp /u/cs401/WEKA/weka.jar weka.classifiers.bayes.NaiveBayes -t train.arff -T test.arff -o >> 3.2output.txt
+sh /u/cs401/WEKA/infogain.sh >> 3.2output.txt
 python buildarff0.py train.twt train.arff 4500
-java -cp /u/cs401/WEKA/weka.jar weka.classifiers.bayes.NaiveBayes -t train.arff -T test.arff -o >> 3.2output.txt
+sh /u/cs401/WEKA/infogain.sh >> 3.2output.txt
+python buildarff0.py train.twt train.arff 5000
+sh /u/cs401/WEKA/infogain.sh >> 3.2output.txt
 python buildarff0.py train.twt train.arff
-java -cp /u/cs401/WEKA/weka.jar weka.classifiers.bayes.NaiveBayes -t train.arff -T test.arff -o >> 3.2output.txt
+sh /u/cs401/WEKA/infogain.sh >> 3.2output.txt
