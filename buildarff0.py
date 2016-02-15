c45a11add12621f94ef97b9f47440924af555c7a
Fang <ailsafangtian@gmail.com>
2016-02-15 14:20:21 -0500

    add

--- buildarff0.py
+++ buildarff0.py
@@ -60,10 +60,12 @@ def aggre_count(s, categories="all"):
 def assign_cate(s, category):
     '''determine which category of words is calling'''
     #if words
-    if category in ["FPP", "SPP", "TPP", "CSC", "DASH", "MSA"]:
+    if category in ["FPP", "SPP", "TPP", "CSC", "DASH"]:#, "MSA"]:
         return count(s,category, post="/")
     elif category=="EL":
         return count(s, category)
+    elif category=="MSA":
+        sum([(s.upper()).count(word.upper()+"/") for word in ca_dict[category]])
     #if tag
     elif category in ["CC", "CO", "PA", "CN", "PN", "ADV", "WW", "PTV"]:
         return count(s,category, pre="/", post=" ")
@@ -93,7 +95,7 @@ def count_type(s, category):
 """
     
 def count(s, category, pre='', post=''):
-    return sum([(s.upper()).count(pre+word.upper()+post) for word in ca_dict[category]])
+    return sum([s.count(pre+word+post) for word in ca_dict[category]])
 
 if __name__ == '__main__':
     
