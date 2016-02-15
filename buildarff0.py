0f0c0b995e3f852a97dfffb8de18b92a0483ea61
Fang <ailsafangtian@gmail.com>
2016-02-15 14:47:27 -0500

    add

--- buildarff0.py
+++ buildarff0.py
@@ -46,10 +46,13 @@ Perfective aspect (has/ have eaten) should be counted as one token
 
 def aggre_count(s, categories="all"):
     '''return list of count of apperence of words in categories in string s'''
+    
     L = []
+    # count all category
     if categories != "all":
         for c in categories:
             L.append(assign_cate(s, c))
+    #count for partial categories
     else:
         for c in ca_list:
             L.append(assign_cate(s, c))
@@ -59,14 +62,15 @@ def aggre_count(s, categories="all"):
 
 def assign_cate(s, category):
     '''determine which category of words is calling'''
-    #if words
+    
+    # words category
     if category in ["FPP", "SPP", "TPP", "CSC", "DASH"]:
         return count(s,category, post="/")
     elif category=="EL":
         return count(s, category)
     elif category=="MSA":
         return sum([(s.upper()).count(word.upper()+"/") for word in ca_dict["MSA"]])
-    #if tag
+    # tag category
     elif category in ["CC", "CO", "PA", "CN", "PN", "ADV", "WW", "PTV"]:
         return count(s,category, pre="/", post=" ")
     #if future tense
@@ -75,39 +79,26 @@ def assign_cate(s, category):
     elif category=="upper":
         sp = s.split(" ")
         return sum([(c.isupper() and c.index("/")>2) for c in sp])
-    
-"""
-def count_word(s,category):
-    '''
-    count the total number of apperence of words in category in string s
-    '''
 
-    
-    elif category=="MSA":
-        return sum([(s.upper()).count(word.upper() + "/") for word in ca_dict[category]])
-       
-
-def count_type(s, category):
-    '''
-    count the total number of apperence tags in category in string s
-    '''
-    return sum([.count("/"+tag+" ") for tag in ca_dict[category]])
-"""
     
 def count(s, category, pre='', post=''):
+    '''count number of appereance of word in string s'''
+
     return sum([s.count(pre+word+post) for word in ca_dict[category]])
 
-if __name__ == '__main__':
-    
+
+def main():
+        
     #check the validity of the input argument
+    #if train with partial training data
     if len(sys.argv) == 4:
         num_data_each_group = sys.argv[3]
         classrange = range(0, int(num_data_each_group)+1) + range(5500, 5500+int(num_data_each_group))
-    else:
+    else: # all training data
         classrange = range(0, 11000)
     
     input_file = open(sys.argv[1], 'rb') #open the tweet file
-    output_file = open(sys.argv[2], 'wb')   
+    output_file = open(sys.argv[2], 'wb') 
     
     # write the relation name
     output_file.write('@relation ' + sys.argv[1][:-5] + '\n\n')
@@ -135,38 +126,36 @@ if __name__ == '__main__':
                       '@attribute number_of_sentence numeric\n' +
                       '@attribute class {0,4}\n\n'+
                       '@DATA\n')
+
+    #=============count features ===============
     
-    #input_file = open('sample.twt', 'rb')
-    #================
-    #Average length of sentences (in tokens)
-    #Average length of tokens, excluding punctuation tokens (in characters)
-    #Number of sentences
-    
-    num_token = 0 #general token
-    char_token = 0 #token of only character
-    num_sen = 0
-    num_char = 0
-    tweet_count=-1
-    first_line=True
+    num_token = 0  #general token
+    char_token = 0  #token of only character
+    num_sen = 0  #number of sentence in tweeter
+    num_char = 0  #number of cahracter in tweeter
+    tweet_count=-1 #position of current tweeter
+    first_line=True  #indicator of first line
 
     for line in input_file.readlines():   # iterates the rows of the file in orders
+        #first line of tweeter
         if line.strip()=="<A=0>" or line.strip()=="<A=4>":
             tweet_count+=1  # the tweet_count th tweet
             if (tweet_count in classrange) and (not first_line):
-                try:
-                    avg_len_sentence = float(num_token)/num_sen
-                    avg_len_token = float(num_char)/char_token
+                try:  #in case tweet doesn't have sentence & tokens 
+                    avg_len_sentence = float(num_token)/num_sen  #Average length of sentences (in tokens)
+                    avg_len_token = float(num_char)/char_token   #Average length of tokens, excluding punctuation tokens (in characters)
                 except ZeroDivisionError:
                     avg_len_sentence=0
                     avg_len_token=0
                  
                 cal+=[avg_len_sentence, avg_len_token, num_sen]  #result
-                output_file.write((str(cal)[1:-1]).replace(" ","") + ", "+ line[3] + "\n")
-                num_token = 0 #general token
-                char_token = 0 #token of only character
+                output_file.write((str(cal)[1:-1]).replace(" ","") + ", "+ line[3] + "\n") #write result to ouput file
+                #----re-initialize----
+                num_token = 0 
+                char_token = 0 
                 num_sen = 0
                 num_char = 0
-                class_ind=line[3]
+                class_ind=line[3]  #class indicator
                 
             elif first_line:
                 first_line=False
@@ -175,30 +164,29 @@ if __name__ == '__main__':
             else:
                 continue
             
-         
+        #content of tweeter
         elif tweet_count in classrange:
             num_sen +=1
-            cal = aggre_count(line)
-            past_num = len(re.findall('((have)|(has)|(had))/[a-zA-Z ]+e(n|d)/', line)) #past time verb
-            #futurn time verb
+            cal = aggre_count(line)  #calculate fieatures of tweeter
+            past_num = len(re.findall('((have)|(has)|(had))/[a-zA-Z ]+e(n|d)/', line)) #number of past time verb
+            #number of futurn time verb
             futurn_num = len(re.findall('going/VBG to/TO [a-zA-Z]+/VB', line))*2 + line.count("will") + line.count("'ll") + line.count("gonna")
-            num_token += len(line.split(" ")) - past_num - futurn_num #number of tokens (all kinds)
+            num_token += len(line.split(" ")) - past_num - futurn_num  #number of tokens (all kinds)
             line = tag.sub('', line)
-            char_token += len((only_alph.sub(' ', line)).split()) - past_num - futurn_num
-            num_char += len(only_alph.sub('', line))
+            char_token += len((only_alph.sub(' ', line)).split()) - past_num - futurn_num  #number of tokens (only char)
+            num_char += len(only_alph.sub('', line))  #number only char in tweeter
 
-    if len(sys.argv) == 3:            
+    if len(sys.argv) == 3: #if all data, write result for last tweeter, otherwise skip       
         avg_len_sentence = float(num_token)/num_sen
         avg_len_token = float(num_char)/char_token
         cal+=[avg_len_sentence, avg_len_token, num_sen]  #result
         output_file.write((str(cal)[1:-1]).replace(" ","") + ", 4\n")
+    #============================
         
-    #=================
-                  
-    
-    #write data
-    
+    #write data   
     input_file.close()
     output_file.close()
-    #nrc saif NLP NRC Emotion Lexicon
 
+
+if __name__ == '__main__':
+    main()
